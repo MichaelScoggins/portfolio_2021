bc171e65ff6205b9cedd29d6a158ff4fa6057da9
Michael Scoggins <rhythmghosts@gmail.com>
2021-01-13 17:11:19 -0600

    set up some sanity backend (schemas) and miscellaneous front end stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,22 @@
 import React from "react";
+import image from "../moonman_relaxer.jpg";
 
 export default function Home() {
-  return <h1>Home Page!</h1>;
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Lost In Space"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1
+          style={{ color: "yellow" }}
+          className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"
+        >
+          Aloha. I'm Michael!
+        </h1>
+      </section>
+    </main>
+  );
 }
---
+++ studio/schemas/post.js
@@ -0,0 +1,55 @@
+export default {
+  title: "Post",
+  type: "document",
+  name: "post",
+  fields: [
+    {
+      title: "Title",
+      name: "title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: [
+        {
+          type: "author",
+        },
+      ],
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    // {
+    //   name: "categories",
+    //   title: "Categories",
+    //   type: "array",
+    //   of: [{ type: "reference", to: { type: "category" } }],
+    // },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+    {
+      name: "body",
+      type: "richText",
+      title: "Body Text",
+    },
+  ],
+};
