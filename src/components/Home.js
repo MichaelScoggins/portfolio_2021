0b989b54b02d6364142379571c54bd9a453af361
Michael Scoggins <rhythmghosts@gmail.com>
2021-01-19 22:43:41 -0600

    fixed it with picture tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,21 @@ import smallImg from "../astronot_gna_make_it.jpg";
 export default function Home() {
   return (
     <main>
-      <img
+      {/* <img
         src={smallImg}
-        srcSet={`${smallImg} 300w, ${largeImg} 1280w`}
+        srcSet={`${smallImg} 100w, ${largeImg} 1280w`}
         alt="astronaut"
         className="absolute object-cover w-full h-full"
-      />
+      /> */}
+      <picture>
+        <source srcset={largeImg} media="(min-width: 1200px)" />
+        {/* <source srcset="medium.jpg" media="(min-width: 800px)" /> */}
+        <img
+          src={smallImg}
+          alt="astronaut"
+          className="absolute object-cover w-full h-full"
+        />
+      </picture>
     </main>
   );
 }
