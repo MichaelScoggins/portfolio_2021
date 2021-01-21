f6efadd604e05b5dfc701a6646f095f2e2daae08
Michael Scoggins <rhythmghosts@gmail.com>
2021-01-21 03:50:18 -0600

    removed portrait img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import landscapeImg from "../moonman_relaxer.jpg";
-import portraitImg from "../virtual_dom_cropped.jpg";
+// import portraitImg from "../virtual_dom_cropped.jpg";
 
 export default function Home() {
   return (
     <main>
       <picture>
-        <source srcset={landscapeImg} media="(min-width: 500px)" />
+        {/* <source srcset={landscapeImg} media="(min-width: 500px)" /> */}
         <img
-          src={portraitImg}
+          src={landscapeImg}
           alt="shoot for the stars chill on the moon"
           className="absolute object-cover w-full h-full"
         />
