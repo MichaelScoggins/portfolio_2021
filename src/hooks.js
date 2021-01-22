01c4b0dc7eb67fde6d19eae15299af4a38faeecf
Michael Scoggins <rhythmghosts@gmail.com>
2021-01-22 14:08:18 -0600

    fixed clickAway

--- src/hooks.js
+++ src/hooks.js
@@ -4,9 +4,9 @@ import { useEffect } from "react";
 export const useOnClickOutside = (ref, handler) => {
   useEffect(() => {
     const listener = (event) => {
-      if (!ref.current || ref.current.contains(event.target)) {
-        return;
-      }
+      // if (!ref.current || ref.current.contains(event.target)) {
+      //   return;
+      // }
       handler(event);
     };
     document.addEventListener("mousedown", listener);
