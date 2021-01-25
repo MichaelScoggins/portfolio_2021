ea5212c87dc1fddb7056386bfaa18a358e359442
Michael Scoggins <rhythmghosts@gmail.com>
2021-01-24 23:28:34 -0600

    more responsiveness tweaking

--- src/components/About.js
+++ src/components/About.js
@@ -28,21 +28,34 @@ export default function About() {
   if (!author) return <div>Loading...</div>;
 
   return (
-    <main className="relative">
-      <img src={buildings} alt="Heart and Mind" className="absolute w-full" />
-      <div className="p-10 lg:pt-48 container mx-auto relative">
-        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-10">
+    <main className="">
+      <img
+        src={buildings}
+        alt="Buildings"
+        className="absolute w-full h-full object-cover"
+      />
+      <div
+        id="about-container"
+        className="p-4 lg:p-10 lg:pt-48 container mx-auto relative"
+      >
+        <section
+          id="about-section"
+          className="bg-green-800 rounded-lg shadow-2xl flex p-10"
+        >
           <img
             src={urlFor(author.authorImage).url()}
             className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
             alt={author.name}
           />
-          <div className="text-lg flex flex-col justify-center">
-            <h1 className="cursive sm:text-base md:text-xl lg:text-5xl text-green-300 mb-4">
+          <div id="author-name" className="flex flex-col justify-center">
+            <h1 className="cursive text-lg lg:text-5xl text-green-300 mb-4">
               Hey there, I'm{" "}
-              <span className="text-green-100">{author.name}</span>
+              <span className="text-green-100">{author.name}!</span>
             </h1>
-            <div className="prose lg:prose-xl text-white">
+            <div
+              id="author-bio"
+              className="text-base lg:text-lg prose lg:prose-xl text-white"
+            >
               <BlockContent
                 blocks={author.bio}
                 projectId="lbo3izt8"
