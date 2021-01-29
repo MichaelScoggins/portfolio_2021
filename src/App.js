d614126ff4e865ed53548d49899cbce78ffdf20f
Michael Scoggins <rhythmghosts@gmail.com>
2021-01-29 13:40:57 -0600

    implementing responsive navbar scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useOnClickOutside } from "./hooks";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
@@ -14,9 +14,31 @@ import { theme } from "./theme";
 
 function App() {
   const [open, setOpen] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(false);
   const node = useRef();
-
   useOnClickOutside(node, () => setOpen(false));
+
+  const handleScroll = () => {
+    // find current scroll position
+    const currentScrollPos = window.pageYOffset;
+
+    // set state based on location info (explained in more detail below)
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos > 70) ||
+        currentScrollPos < 10
+    );
+
+    // set state to new scroll position
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
     <ThemeProvider theme={theme}>
       <>
@@ -33,7 +55,11 @@ function App() {
             <Menu open={open} setOpen={setOpen} />
           </nav>
           <div id="html-container">
-            <div id="nav-container" className="pb-20">
+            <div
+              id="nav-container"
+              className="pb-20"
+              style={{ top: visible ? "0" : "-60px", transition: "top .6s" }}
+            >
               <NavBar />
             </div>
             <div id="body-container">
--- src/hooks.js
+++ src/hooks.js
@@ -9,7 +9,9 @@ export const useOnClickOutside = (ref, handler) => {
       }
       handler(event);
     };
+
     document.addEventListener("mousedown", listener);
+
     return () => {
       document.removeEventListener("mousedown", listener);
     };
