d22388a342b738dcb86fbf4c607e95ca5d66d130
Michael Scoggins <rhythmghosts@gmail.com>
2021-01-29 10:41:19 -0600

    finished mirroring the burger menu

--- src/components/Burger/Burger.styled.js
+++ src/components/Burger/Burger.styled.js
@@ -29,17 +29,17 @@ export const StyledBurger = styled.button`
     transform-origin: 1px;
 
     :first-child {
-      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+      transform: ${({ open }) => (open ? "rotate(0)" : "rotate(45deg)")};
     }
 
     :nth-child(2) {
-      opacity: ${({ open }) => (open ? "0" : "1")};
+      opacity: ${({ open }) => (open ? "1" : "0")};
       transform: ${({ open }) =>
-        open ? "translateX(-20px)" : "translateX(0)"};
+        open ? "translateX(0)" : "translateX(-20px)"};
     }
 
     :nth-child(3) {
-      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
+      transform: ${({ open }) => (open ? "rotate(0)" : "rotate(-45deg)")};
     }
   }
 `;
